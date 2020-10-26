c1ae543db180227e95bc4bd87239d56677892ca5
Ayush Arora <aarora7@dons.usfca.edu>
2020-10-25 22:40:22 -0700

    map fixed

--- index1.js
+++ index1.js
@@ -10,65 +10,64 @@ var unemployment = d3.map();
 var path = d3.geoPath();
 
 var x = d3.scaleLinear()
+    .domain([0, 10, 20, 30, 40, 50, 60, 70, 100])
+    .rangeRound([600, 860]);
+
+
+var x1 = d3.scaleLinear()
     .domain([1, 10])
     .rangeRound([600, 860]);
 
-var color = d3.scaleThreshold()
+var color1 = d3.scaleThreshold()
     .domain(d3.range(1 , 10))
     .range(d3.schemeBlues[9]);
 
+var color = d3.scaleThreshold()
+    .domain([0, 10, 20, 30, 40, 50, 60, 70, 100])
+    .range(d3.schemeBlues[9]);
 
 var g = svg.append("g")
     .attr("class", "key")
     .attr("transform", "translate(0,40)");
 
-g.selectAll("rect")
-    .data(color.range().map(function(d) {
-        d = color.invertExtent(d);
-        if (d[0] == null) d[0] = x.domain()[0];
-        if (d[1] == null) d[1] = x.domain()[1];
-        return d;
-    }))
-    .enter().append("rect")
-    .attr("height", 8)
-    .attr("x", function(d) { return x(d[0]); })
-    .attr("width", function(d) { return x(d[1]) - x(d[0]); })
-    .attr("fill", function(d) { return color(d[0]); });
+svg
+    .append("g")
+    .attr("transform", "translate(560,20)")
+    .append(() => legend({
+        color: d3.scaleQuantize([0, 99], d3.schemeBlues[9]),
+        title: "Vote Percentage (->  increasing)"
+    }));
 
-g.append("text")
-    .attr("class", "caption")
-    .attr("x", x.range()[0])
-    .attr("y", -6)
-    .attr("fill", "#000")
-    .attr("text-anchor", "start")
-    .attr("font-weight", "bold")
-    .text("Vote Percentage (->  increasing)");
-
+let percentage = []
 var promises = [
     d3.json("https://d3js.org/us-10m.v1.json"),
     d3.tsv("vote_percentage.tsv", function(d) {
-        // color.domain([
-        //     d3.min(d, function(d) { return +d.percentage; }),
-        //     d3.max(d, function(d) { return +d.percentage; })
-        // ]);
+        percentage.push(+d.percentage)
         unemployment.set(d.id, +d.percentage); })
 ]
 
 Promise.all(promises).then(ready)
 
 function ready([us]) {
+
     svg.append("g")
         .attr("class", "counties")
         .selectAll("path")
         .data(topojson.feature(us, us.objects.counties).features)
         .enter().append("path")
         .attr("fill", function(d) {
-            return color(d.percentage = unemployment.get(d.id));
+            if(unemployment.get(d.id))
+                return color(d.percentage = unemployment.get(d.id));
+            else
+                return color(d.percentage = 0);
         })
         .attr("d", path)
         .append("title")
         .text(function(d) {
-            return d.percentage == undefined ? 50 + "%":+ d.percentage + "%";
+            if(unemployment.get(d.id)){
+                return d.percentage === undefined ? "50%":+ d.percentage + "%";
+            }
+            return "NA";
         });
 
     svg.append("path")
@@ -77,10 +76,146 @@ function ready([us]) {
         .attr("d", path);
 
     // Adding chart label
+    // Adding chart label
     svg.append("text")
         .attr("class", "chart-label")
         .attr("x",  width / 5)
         .attr("y", 12)
-        .text("Top 6 Cities with highest vote count for Bernie Sanders (2016)")
+        .text("Vote count for Bernie Sanders (2016)")
+}
 
+// Ref
+function legend({
+                    color,
+                    title,
+                    tickSize = 6,
+                    width = 320,
+                    height = 44 + tickSize,
+                    marginTop = 18,
+                    marginRight = 0,
+                    marginBottom = 16 + tickSize,
+                    marginLeft = 0,
+                    ticks = width / 64,
+                    tickFormat,
+                    tickValues
+                } = {}) {
+
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .style("overflow", "visible")
+        .style("display", "block");
+
+    let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+    let x;
+
+    // Continuous
+    if (color.interpolate) {
+        const n = Math.min(color.domain().length, color.range().length);
+
+        x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
+
+        svg.append("image")
+            .attr("x", marginLeft)
+            .attr("y", marginTop)
+            .attr("width", width - marginLeft - marginRight)
+            .attr("height", height - marginTop - marginBottom)
+            .attr("preserveAspectRatio", "none")
+            .attr("xlink:href", ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
+    }
+
+    // Sequential
+    else if (color.interpolator) {
+        x = Object.assign(color.copy()
+                .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+            {range() { return [marginLeft, width - marginRight]; }});
+
+        svg.append("image")
+            .attr("x", marginLeft)
+            .attr("y", marginTop)
+            .attr("width", width - marginLeft - marginRight)
+            .attr("height", height - marginTop - marginBottom)
+            .attr("preserveAspectRatio", "none")
+            .attr("xlink:href", ramp(color.interpolator()).toDataURL());
+
+        // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+        if (!x.ticks) {
+            if (tickValues === undefined) {
+                const n = Math.round(ticks + 1);
+                tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+            }
+            if (typeof tickFormat !== "function") {
+                tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+            }
+        }
+    }
+
+    // Threshold
+    else if (color.invertExtent) {
+        const thresholds
+            = color.thresholds ? color.thresholds() // scaleQuantize
+            : color.quantiles ? color.quantiles() // scaleQuantile
+                : color.domain(); // scaleThreshold
+
+        const thresholdFormat
+            = tickFormat === undefined ? d => d
+            : typeof tickFormat === "string" ? d3.format(tickFormat)
+                : tickFormat;
+
+        x = d3.scaleLinear()
+            .domain([-1, color.range().length - 1])
+            .rangeRound([marginLeft, width - marginRight]);
+
+        svg.append("g")
+            .selectAll("rect")
+            .data(color.range())
+            .join("rect")
+            .attr("x", (d, i) => x(i - 1))
+            .attr("y", marginTop)
+            .attr("width", (d, i) => x(i) - x(i - 1))
+            .attr("height", height - marginTop - marginBottom)
+            .attr("fill", d => d);
+
+        tickValues = d3.range(thresholds.length);
+        tickFormat = i => thresholdFormat(thresholds[i], i);
+    }
+
+    // Ordinal
+    else {
+        x = d3.scaleBand()
+            .domain(color.domain())
+            .rangeRound([marginLeft, width - marginRight]);
+
+        svg.append("g")
+            .selectAll("rect")
+            .data(color.domain())
+            .join("rect")
+            .attr("x", x)
+            .attr("y", marginTop)
+            .attr("width", Math.max(0, x.bandwidth() - 1))
+            .attr("height", height - marginTop - marginBottom)
+            .attr("fill", color);
+
+        tickAdjust = () => {};
+    }
+
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(d3.axisBottom(x)
+            .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+            .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+            .tickSize(tickSize)
+            .tickValues(tickValues))
+        .call(tickAdjust)
+        .call(g => g.select(".domain").remove())
+        .call(g => g.append("text")
+            .attr("x", marginLeft)
+            .attr("y", marginTop + marginBottom - height - 6)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text(title));
+
+    return svg.node();
 }
